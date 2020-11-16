5e3297fc52a087196e72cc492db03d46144514e2
nemanja.radulovic <nemanja.radulovic@NRADULOVIC-NB>
2020-11-16 14:04:24 +0100

    First commit, simple web server

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject alati "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.2"]]
+  :repl-options {:init-ns alati.core}
+  :main alati.core)
---
+++ src/alati/core.clj
@@ -0,0 +1,14 @@
+(ns alati.core
+  (:require [ring.adapter.jetty :as webserver]))
+
+(defn -main
+  "A very simple web server using Ring & Jetty"
+  [port-number]
+  (webserver/run-jetty
+   (fn [request]
+     {:status  200
+      :headers {}
+      :body    "<h1>Hello, Clojure World</h1>
+                 <p>Welcome to your first Clojure app.</p>"})
+   {:port  (Integer. port-number)
+    :join? false}))
